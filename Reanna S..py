056cf694ce67142c7c61513ca3af98370333e7f3
Kandylion401 <52932014+Kandylion401@users.noreply.github.com>
2019-07-15 15:53:58 -0400

    Add files via upload

---
+++ Reanna S..py
@@ -0,0 +1,50 @@
+#new file
+import pandas
+print("I am learning to code on Python today.")
+print("I am 14 years old.")
+"hello world"
+5*2
+5/2
+5-2
+5**2
+8/9*3
+print("I am learning to code right now.")
+print("My name is Reanna.")
+print ("53")
+print ("How is your day?")
+print(The division of 5/2 is 2.5)
+print(The division of 5/3 is)
+print(The division of 5/3 is two point five)
+print("The division of 5/2 is",)
+def plus (a,b)
+def plus (8,6)
+plus(a,b,c):
+    plus= a + b + c
+    
+sum(8,5
+sum(8+5)
+sum(a,b,c):
+
+    plus= a + b + c
+    8+5
+add(a,b)
+
+def plus(a,b,c):
+    plus = a + b + c
+    print(plus)    
+ (8+5)
+plus(2,4)
+
+#create function to find difference of two numbers
+def Subtract (a,b):
+    Subtract = a - b
+    print(Subtract)
+    
+    Subtract(80,60)
+    
+#create function to find area of a triangle
+def Area(b,h):
+    Area = 1/2*b*h
+    print(Area)
+
+Area(10,10)        
