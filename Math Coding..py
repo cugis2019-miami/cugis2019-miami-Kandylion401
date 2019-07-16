4046b9266175c079ef700c95c2ea67ad22906bdb
Kandylion401 <52932014+Kandylion401@users.noreply.github.com>
2019-07-16 16:04:00 -0400

    Add files via upload

---
+++ Math Coding..py
@@ -0,0 +1,84 @@
+#math coding
+import math
+22/7
+dir(math)
+
+math.pi
+
+math.sqrt(10)
+
+math.factorial(0)
+
+math.factorial(5)
+
+math.sin(2.5)
+
+math.pow(80)
+
+math.remainder(10/7)
+
+math.remainder(10,7)
+
+math.remainder(12,7)
+
+math.pow(2,1/4)
+
+math.pow(5,2)
+
+math.pow(2.5,4.9)
+
+math.pow(0,0)
+
+math.remainder(9,7)
+import math
+22/7
+dir(math)
+
+i=3
+k=9
+
+print("The cubic root of the",k,"is",i)
+
+math.pow(90,1/3)
+        
+math.pow(a,b):
+    math.pow(90,1/3)
+    print(math.pow)
+    
+a=90
+b=1/3
+
+print("The cubic root of the",a,"is",b)
+def cube(y)
+def cube math.pow(90,1/3):
+    cube math.pow(a,b)
+    print("The cube root of",y,"is",cube)
+cube(8)
+math.pow(a,b):
+    math.pow(a,1/3)
+    
+def cube = math.pow(y,1/3
+                    
+def cubicroot(a):
+    
+print("Please enter a value for x")
+#finding cube
+def cube(x):
+    cube=math.pow(x,1/3)
+    print("The cube root of",x,"is",cube)
+    
+cube(27)
+
+import math
+print("Please enter a value")
+y=20
+
+cube=math.pow(y,(1/3))
+print("The cube root of",y,"is",cube)
+cube(y)
+
+q ="Please enter a value to the variable t"
+print(q,cube(27))
+
+
+
